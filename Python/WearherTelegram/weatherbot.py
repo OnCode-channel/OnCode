4aa7022932bc5aece39121233b34ebea12063717
Dmitry Karpenko <dkarp3ncko@yandex.ru>
2022-02-26 20:54:50 +0500

    Update weatherbot.py

--- Python/WearherTelegram/weatherbot.py
+++ Python/WearherTelegram/weatherbot.py
@@ -2,7 +2,7 @@ import telebot
 from pyowm import OWM
 from pyowm.utils.config import get_default_config
 
-bot = telebot.TeleBot("API-key")
+bot = telebot.TeleBot("telegram API-key")
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -20,7 +20,7 @@ def test(message):
 		config_dict = get_default_config()
 		config_dict['language'] = 'ru'
 
-		owm = OWM('c52242b2d4d80ae028d6e97b46ce1c39', config_dict)
+		owm = OWM('owm api-key', config_dict)
 		mgr = owm.weather_manager()
 		observation = mgr.weather_at_place(place)
 		w = observation.weather
